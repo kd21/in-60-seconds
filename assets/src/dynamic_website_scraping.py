4cca66b577caccee5e220afaad0d8303843f4f25
kd21 <bird.ocean.21@gmail.com>
2019-01-07 18:22:39 +0900

    Update dynamic_website_scraping.py

--- assets/src/dynamic_website_scraping.py
+++ assets/src/dynamic_website_scraping.py
@@ -1,1 +1,11 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
+url = "https://www.amazon.co.jp/gp/bestsellers/books/ref=zg_bs_books_pg_1?ie=UTF8&pg=1"
+browser = webdriver.PhantomJS()
+browser.implicitly_wait(3)
+browser.get(url)
+html_source = browser.page_source
+bs_obj = BeautifulSoup(html_source)
+books = [a.text for a in bs_obj.select("div.a-fixed-left-grid-col div.p13n-sc-truncated")]
